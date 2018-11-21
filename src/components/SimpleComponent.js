4ff3289876a78bc5cb419d2edc0e0a0690a9f5a2
Bruno Garcia Gonzalez <brunogarciagonzalez@outlook.com>
2018-11-21 10:54:23 -0500

    renamed eObjArray and added comments

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -5,33 +5,36 @@ class SimpleComponent extends React.Component {
     super();
 
     this.state = {
-      toCheck: []
+      // remember to explore the contents of this array as it gets logged to console
+      eObjArray: []
     };
   }
 
   handleClick = e => {
+    // run the app with and without e.persist() to see the difference in the console!
     // e.persist()
-    if (this.state.toCheck.includes(e)){
-      // eObj already in array, do nothing
+
+    if (this.state.eObjArray.includes(e)){
+      // eObj already in array, do not add to eObjArray
       console.warn("eObj not added to arr")
 
       // what is below will equate to true because:
       // even though we are comparing two variables, they both point to the same object in memory
 
-      console.log("comparison result (is obj in Arr same as current eObj?):", this.state.toCheck[0] ===e)
+      console.log("comparison result (is obj in Arr same as current eObj?):", this.state.eObjArray[0] ===e)
 
       // =======
 
       // what is below will equate to true because:
       // since it is same object in memory, the keys of object in the array have been updated with the new values, in order to be passed into this callback this.handleClick!
 
-      // console.log("in state:", this.state.toCheck[0].dispatchConfig)
+      // console.log("in state:", this.state.eObjArray[0].dispatchConfig)
       // console.log("arg:", e.dispatchConfig)
     } else {
       console.warn("eObj yes added to arr")
       this.setState({
-        toCheck: [...this.state.toCheck, e]
-      }, () => console.log(this.state.toCheck))
+        eObjArray: [...this.state.eObjArray, e]
+      }, () => console.log(this.state.eObjArray))
     }
 
 
@@ -39,9 +42,12 @@ class SimpleComponent extends React.Component {
 
   render() {
     return (
-      <button onClick={this.handleClick}>
-        Trigger onClick Event
-      </button>
+      <div>
+        <p>Open the console to see the app at work!</p>
+        <button onClick={this.handleClick}>
+          Trigger onClick Event
+        </button>
+      </div>
     )
   }
 }
